e06befa27eb8c7fd24ad71eae6d1043dcba5d806
michel <michel@papiamentos-MacBook-Pro.local>
2020-04-27 16:24:59 -0400

    add lambda files

--- cicd-pipeline/notification_lambda.tf
+++ cicd-pipeline/notification_lambda.tf
@@ -46,7 +46,7 @@ resource "aws_lambda_function" "notification-lambda-function" {
   # The filebase64sha256() function is available in Terraform 0.11.12 and later
   # For Terraform 0.11.11 and earlier, use the base64sha256() function and the file() function:
   # source_code_hash = "${base64sha256(file("lambda_function_payload.zip"))}"
-  source_code_hash = filebase64sha256(var.lambda-zip-file)
+  source_code_hash = filebase64sha256("${path.module}/${var.lambda-zip-file}")
 
   runtime = "python3.7"
 
