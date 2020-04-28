a23a788f8ccfe09af70eed58fbc269e26aeb8dec
michel <1024836+michel-lacle@users.noreply.github.com>
2020-04-28 09:46:02 -0400

    refactoring notifications out

--- main.tf
+++ main.tf
@@ -11,14 +11,14 @@ module "cicd-pipeline-master-branch" {
   repository-name = aws_codecommit_repository.codecommit-repository.repository_name
 
 }
-
+/*
 module "cicd-pipeline-master-branch-notifications" {
   source = "./cicd-notifications"
 
   codepipeline-name = module.cicd-pipeline-master-branch.codepipeline-name
   s3-bucket-name = module.cicd-pipeline-master-branch.cicd-artifact-bucket-name
   slack-url = var.slack-url-succeeded
-}
+}*/
 
 module "cicd-pipeline-dev-branch" {
   source = "./cicd-pipeline"
